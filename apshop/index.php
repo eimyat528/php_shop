02e8cf312581c588137b647e30a0de0bc1e4c6e2
Ei Myat <74184859+eimyat528@users.noreply.github.com>
2024-01-13 09:03:50 +0630

    design commit

--- apshop/index.php
+++ apshop/index.php
@@ -1,4 +1,8 @@
-<?php include('header.html') ?>
+<?php 
+			include('header.html') 
+			
+			?>
+
 				<!-- End Filter Bar -->
 				<!-- Start Best Seller -->
 				<section class="lattest-product-area pb-40 category-list">
@@ -80,24 +84,20 @@
 						</div>
 					</div>
 				</section>
-				<!-- End Best Seller -->
-			</div>
+
+			
+						<!-- End Best Seller -->
+				</div>
 		</div>
 	</div>
+	            <?php 
+                  include('footer.php') 
+  
+                ?>
 
-
-
+			
 	<!-- start footer Area -->
-	<footer class="footer-area section_gap">
-		<div class="container">
-			<div class="footer-bottom d-flex justify-content-center align-items-center flex-wrap">
-				<p class="footer-text m-0"><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart-o" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
-<!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-</p>
-			</div>
-		</div>
-	</footer>
+
 	<!-- End footer Area -->
 
 	<script src="js/vendor/jquery-2.2.4.min.js"></script>
